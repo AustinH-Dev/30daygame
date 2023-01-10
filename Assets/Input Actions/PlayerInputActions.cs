f065767ddfce9d3029a0019a70f2f3c0e946ccf4
matcuth <44516921+matcuth@users.noreply.github.com>
2023-01-10 04:29:07 -0500

    Implemented a groundcheck, fixed wall friction, added dash, cleaned up movement mechanics

--- Assets/Input Actions/PlayerInputActions.cs
+++ Assets/Input Actions/PlayerInputActions.cs
@@ -62,6 +62,15 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""fff2cac4-e85a-4161-a1ee-66c070328e59"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -304,6 +313,28 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""Gamepad"",
                     ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""40eac8c3-7847-43cc-b08e-1f82347a05f9"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0ae03ced-cf8c-46c6-8d9b-cc13b042838b"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -895,6 +926,7 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -970,6 +1002,7 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Dash;
     public struct PlayerActions
     {
         private @PlayerInputActions m_Wrapper;
@@ -978,6 +1011,7 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -999,6 +1033,9 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
                 @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Dash.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
+                @Dash.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnDash;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -1015,6 +1052,9 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Dash.started += instance.OnDash;
+                @Dash.performed += instance.OnDash;
+                @Dash.canceled += instance.OnDash;
             }
         }
     }
@@ -1175,6 +1215,7 @@ public partial class @PlayerInputActions : IInputActionCollection2, IDisposable
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnDash(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/Scripts/Controls/PlayerController.cs
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,26 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody body;
     [SerializeField] GameObject camHolder;
-    [SerializeField] float speed, sensitivity, baseGravity = 9.8f, jumpForce, gravityReducer;
+    [SerializeField] Camera cam;
+    [SerializeField] float speed, sensitivity, groundDistance = .6f, baseGravity = 9.8f;
+    [SerializeField] float jumpForce, gravityReducer;
+    [SerializeField] float dashForce = 10f, dashRechargeTime = 2f, maxAdditiveDashAngle = 45f;
+    [SerializeField] int multiJumpMax = 1, dashMax = 1;
     private Vector2 move, look;
     private float actualGravity, lookRotation;
+    private int multiJumpCount, dashCount;
+    bool isGrounded = false;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("Currently Locking Cursor in the PlayerController Start");
+        Cursor.lockState = CursorLockMode.Locked;
+        multiJumpCount = multiJumpMax;
+        dashCount = dashMax;
+        actualGravity = baseGravity;
     }
 
     // Update is called once per frame
@@ -27,14 +38,32 @@ public class PlayerController : MonoBehaviour
             //reset base gravity
             actualGravity = baseGravity;
         }
+        //Check if we are grounded
+        isGrounded = CheckGround();
+    }
+
+    private bool CheckGround()
+    {
+        //Make a raycast
+        Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+        RaycastHit hitData;
+        //if our raycast hits an object
+        if (Physics.Raycast(groundCheckRay, out hitData, groundDistance))
+        {
+            //we are grounded
+            //reset our multijumps
+            multiJumpCount = multiJumpMax;
+            return true;
+        }
+        //otherwise we arent
+        else return false;
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(move.x, 0, move.y);
         body.AddRelativeForce(movement * speed);
-        body.AddForce(Vector3.down * actualGravity);
-
+        body.AddForce(Vector3.down * actualGravity, ForceMode.Acceleration);
     }
 
     private void LateUpdate()
@@ -62,14 +91,19 @@ public class PlayerController : MonoBehaviour
         //if we start holding the button
         if (context.phase == InputActionPhase.Performed)
         {
-            //cancel out any negative velo
-            body.velocity = new Vector3(body.velocity.x, 0.1f, body.velocity.z);
-            //perform a 1 time impulse of force
-            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            //reduce gravity while button held
-            actualGravity = baseGravity / gravityReducer;
+            //we check for grounding or multijumps remaining
+            if (isGrounded || multiJumpCount > 0)
+            {
+                //when we arent grounded, we decrement the multijump count
+                if (!isGrounded) multiJumpCount--;
+                //cancel out any negative velo
+                body.velocity = new Vector3(body.velocity.x, 0.1f, body.velocity.z);
+                //perform a 1 time impulse of force
+                body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                //reduce gravity while button held
+                actualGravity = baseGravity / gravityReducer;
+            }
         }
-
         //when we release the button
         else if (context.phase == InputActionPhase.Canceled)
         {
@@ -78,4 +112,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        //when we hit the button and have a dash available
+        if (context.phase == InputActionPhase.Started && dashCount > 0)
+        {
+            /*An interesting mechanic idea is that we go even faster if we dash in the direction we are facing, 
+            but to ensure a minimum level of dash power, if we are facing in another direction we will first remove
+            all velocity to ensure the dash is still powerful. this allows for escalating speed limited by physics*/
+
+            //If we the angle between our velo and the direction we are facing is not within the range
+            if (Vector3.Angle(cam.transform.forward, body.velocity) > maxAdditiveDashAngle)
+            {
+                //we will first reduce the velo to 0
+                body.velocity = (Vector3.zero);
+            }
+            //we add our dash force to the player
+            body.AddForce(cam.transform.forward * dashForce, ForceMode.Impulse);
+            Debug.Log("applying force" + cam.transform.forward * dashForce + " from direction " + cam.transform.forward);
+            //and decrement the dash count
+            dashCount--;
+            //then begin a coroutine to recharge our dash
+            StartCoroutine(RechargeDash());
+        }
+    }
+
+    IEnumerator RechargeDash()
+    {
+        //we wait for the recharge time
+        yield return new WaitForSeconds(dashRechargeTime);
+        //then we add a charge, up to the max count. technically clamp shouldnt be needed, as we only
+        //add a charge when we remove one, but we should still be safe. this also allows for pickups
+        //that would refresh dash.
+        dashCount = Mathf.Clamp(dashCount + 1, 0, dashMax);
+    }
 }
